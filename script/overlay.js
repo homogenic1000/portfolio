fd3bf4b35fdf3cc65e20e5d915cf3a7e55637fb0
matheodelessert <hello@matheodelessert.ch>
2025-11-08 21:31:22 +0100

    added eventlistener for quitting the overlay with escape in overlay.js

--- script/overlay.js
+++ script/overlay.js
@@ -1,10 +1,10 @@
 addEventListener('keydown', function(event) {
   if (event.key === 'Escape' || event.key === ' ') {
     console.log('key pressed')
-    const overlays = document.querySelectorAll('.o-active');
-    overlays.forEach(element => {
-      element.classList.remove('o-active');
-      element.classList.add('o-inactive')
-    });
+    const container = document.getElementById('o-container-active');
+    if (container) {
+      container.classList.remove('o-container-active');
+      container.classList.add('o-container-inactive');
+    }
   }
 });
