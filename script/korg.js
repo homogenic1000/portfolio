0070c6c73ccea9cd2509759d1ce74902b25e6b5f
matheodelessert <hello@matheodelessert.ch>
2026-01-07 16:54:35 +0100

    Good layout for korg next ytep add threeJS the troubleshooting was from the style of the hero section

--- script/korg.js
+++ script/korg.js
@@ -1,5 +1,9 @@
 const titleD = document.getElementById("title-d");
 const korgText = document.getElementById("korg-text");
+const animation = document.getElementById("animation-bag");
+const korgbody = document.getElementById("korg-body");
+const hero = document.getElementById("hero-section");
+
 // add a variable of image of the cd
 // maybe using three.js ? :)
 // put the korgText at the top of the page
@@ -9,6 +13,12 @@ function onKorgClick() {
   document.body.style.backgroundColor = "black";
   document.body.style.color = "white";
   titleD.style.display = "none";
-  korgText.style.display = "block";
-
+  Matter.Composite.remove(engine.world, ground);
+  sandwich.style.display = "none";
+  animation.style.display = "none";
+  korgbody.style.display = "flex";
+  korgText.style.display = "flex";
+  hero.style.position = "static";
+  hero.style.textAlign = "left";
+  hero.style.justifyContent = "flex-start";
 }
