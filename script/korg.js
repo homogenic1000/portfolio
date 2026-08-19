0f5a127c58f72cfd731e101d8c873bb5581c994d
Mathöo Delessert <mathoodelessert@Matheos-MacBook-Pro.local>
2026-08-19 12:32:35 +0200

    Update korg.js

--- script/korg.js
+++ script/korg.js
@@ -12,10 +12,10 @@ const body = document.body;
 
 const sandwich = document.getElementById("sandwich");
 
-window.onKorgClick = function() {
+window.onKorgClick = function () {
   titleD.style.display = "none";
   Matter.Composite.remove(engine.world, ground);
-    setTimeout(() => {
+  setTimeout(() => {
     Matter.Composite.remove(engine.world, [korg, filtre, tabac, pamplemousse, aboutme]);
   }, 2000); // délai de 500ms avant d'exécuter la fonction
 
@@ -28,61 +28,37 @@ window.onKorgClick = function() {
   hero.style.textAlign = "left";
   hero.style.justifyContent = "flex-start";
 
+
+  function resetAll() {
+    resetState();
+
+    // Get the physics objects and add them back to the world
+    if (typeof getObjects === "function") {
+      const currentBodies = getObjects();
+      // Filter out undefined objects and add them back
+      addToWorld(currentBodies.filter(b => b));
+    }
+
+    resetAnimation();
+  }
+  backButton.addEventListener("click", resetAll);
+
   
+  function resetState() {
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+    titleD.style.display = "block";
 
+    if (typeof engine !== "undefined" && typeof ground !== "undefined") {
+      Matter.Composite.add(engine.world, ground);
+    }
 
+    sandwich.style.display = "none";
+    animation.style.display = "block";
+    korgbody.style.display = "none";
 
-
-
-var typewriter = new Typewriter(titleD, {
-    loop: false,
-    delay: 10,
-    cursor: '▮',
-});
-
-
-
-  typewriter
-    .pauseFor(2500)
-    .typeString('This project is one of my favorite, i had so much fun designing it and manufacturing it. I made a CD of a track’s􀫀 friends that’s called “Korg e-LIVEsEx”, it’s an homage to this song because thanks to it i have met Nelson the compositer of this song.')
-    .start();
-
-
-}
-
-
-
-function resetAll() {
-  resetState();
-
-  // Get the physics objects and add them back to the world
-  if (typeof getObjects === "function") {
-    const currentBodies = getObjects();
-    // Filter out undefined objects and add them back
-    addToWorld(currentBodies.filter(b => b));
+    hero.style.position = "";
+    hero.style.textAlign = "";
+    hero.style.justifyContent = "space-between";
   }
-
-  resetAnimation();
-}
-backButton.addEventListener("click", resetAll);
-
-
-
-
-function resetState() {
-  document.body.style.backgroundColor = "white";
-  document.body.style.color = "black";
-  titleD.style.display = "block";
-
-  if (typeof engine !== "undefined" && typeof ground !== "undefined") {
-    Matter.Composite.add(engine.world, ground);
-  }
-
-  sandwich.style.display = "none";
-  animation.style.display = "block";
-  korgbody.style.display = "none";
-
-  hero.style.position = "";
-  hero.style.textAlign = "";
-  hero.style.justifyContent = "space-between";
 }
