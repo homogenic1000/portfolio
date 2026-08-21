f324a21e2b06669c4df9f1a894067f860b9725e9
Mathöo Delessert <mathoodelessert@Matheos-MacBook-Pro.local>
2026-08-21 13:17:20 +0200

    woops

--- script/objects.js
+++ script/objects.js
@@ -1,7 +1,7 @@
 // objects.js - Gestion des objets qui tombent
 
-const spawnX = 150;
-const spawnY = 240;
+spawnX = 150;
+spawnY = 240;
 const resitutionValue = 0.9;
 const frictionValueAir = 0.02;
 const frictionValue = 0.05;
